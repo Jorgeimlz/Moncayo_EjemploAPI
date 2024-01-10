0b8e96108312c3b06d4f5952ce75eeee90a88fc3
Jorge Isaac Moncayo Loaiza <jorge.moncayo.loaiza@udla.edu.ec>
2024-01-10 07:43:23 -0500

    Agregar archivos de proyecto.

---
+++ Moncayo_EjemploAPI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Moncayo_EjemploAPI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Moncayo_EjemploAPI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Moncayo_EjemploAPI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
